1414cbd6d0732673923d1c73b033afc3a8863e88
unknown <andreamira-10@outlook.com>
2021-08-18 17:16:51 -0500

    variable 4 con valor 20% creada

--- notas.py
+++ notas.py
@@ -3,4 +3,6 @@ porcentajeNota1=(nota1*10)/100
 nota2=int(input("Ingrese la 2° Nota: "))
 porcentajeNota2=(nota2*10)/100
 nota3=int(input("Ingrese la 3° Nota: "))
-porcentajeNota2=(nota2*15)/100
+porcentajeNota3=(nota3*15)/100
+nota4=int(input("Ingrese la 4° Nota: "))
+porcentajeNota4=(nota4*20)/100
