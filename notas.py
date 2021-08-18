c99835cee3c083210d7e3e94ce67c7d660f30fb4
unknown <ejaramillo62@misena.edu.co>
2021-08-18 17:40:03 -0500

    se agrego nota 5 con porcentaje de 45%

--- notas.py
+++ notas.py
@@ -6,3 +6,13 @@ nota3=int(input("Ingrese la 3° Nota: "))
 porcentajeNota3=(nota3*15)/100
 nota4=int(input("Ingrese la 4° Nota: "))
 porcentajeNota4=(nota4*20)/100
+nota5=int(input("Ingrese la 5° Nota: "))
+porcentajeNota5=(nota5*45)/100
+print("el 10% de la nota1 es : ",porcentajeNota1)
+print("el 10% de la nota2 es : ",porcentajeNota2)
+print("el 15% de la nota3 es : ",porcentajeNota3)
+print("el 20% de la nota4 es : ",porcentajeNota4)
+print("el 45% de la nota es : ",porcentajeNota5)
+
+resultadoTotal=porcentajeNota1+porcentajeNota2+porcentajeNota3+porcentajeNota4+porcentajeNota5
+print("la nota final es : ",resultadoTotal)
