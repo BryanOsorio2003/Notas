69b47f6cbf34f71005509b191aa737adc9624466
unknown <brycrima23@gmail.com>
2021-08-18 17:01:52 -0500

    variable nota 3 con 15% de valor

--- notas.py
+++ notas.py
@@ -1,14 +1,6 @@
-print("bryan")
-print("steven")
-print("osorio")
-print("zuleta")
-print("elian")
-print("jaramillo")
-print("rojas")
-print("kleverma")
-print("salazar")
-print("florez")
 nota1=int(input("Ingrese el 1° Nota: "))
 porcentajeNota1=(nota1*10)/100
 nota2=int(input("Ingrese la 2° Nota: "))
 porcentajeNota2=(nota2*10)/100
+nota3=int(input("Ingrese la 3° Nota: "))
+porcentajeNota2=(nota2*15)/100
