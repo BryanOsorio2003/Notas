5bcf27076abf7fb51cb88342e2c2745108478733
Kleverman Salazar <kleverman1023chess@gmail.com>
2021-08-18 21:26:20 -0500

    v1: Se creo las variables nota1 y nota2 ademas de calcular el porcentaje de ellas

--- notas.py
+++ notas.py
@@ -8,3 +8,7 @@ print("rojas")
 print("kleverma")
 print("salazar")
 print("florez")
+nota1=int(input("Ingrese el 1° Nota: "))
+porcentajeNota1=(nota1*10)/100
+nota2=int(input("Ingrese la 2° Nota: "))
+porcentajeNota2=(nota2*10)/100
